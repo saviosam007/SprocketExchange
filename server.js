1a16a018e6047107eae66bc2b9fa9911c41d7b75
saviosam007 <saviosam007@gmail.com>
2016-08-23 14:14:57 -0500

    added server js file

--- server.js
+++ server.js
@@ -6,11 +6,22 @@ var app     = express();
 var path    = require("path");
 
 app.use(express.static(__dirname + '/app'));
+/*
 app.get('/',function(req,res){
     res.sendFile(path.join(__dirname+'/index.html'));
     //__dirname : It will resolve to your project folder.
 });
 
 app.listen(3000);
+*/
 
+
+app.set('port', (process.env.PORT || 5000));
+
+//For avoidong Heroku $PORT error
+app.get('/', function(request, response) {
+    res.sendFile(path.join(__dirname+'/index.html'));
+}).listen(app.get('port'), function() {
+    console.log('App is running, server is listening on port ', app.get('port'));
+});
 console.log("Running at Port 3000");
